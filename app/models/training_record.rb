8399ab50b3772e8286813284a35b401b4ffe962a
ryouma21 <zuotenglengzhen58@gmail.com>
2025-11-27 23:13:13 +0900

    削除エラー解消のため未実装のform_check関連をコメントアウト

--- app/models/training_record.rb
+++ app/models/training_record.rb
@@ -1,6 +1,6 @@
 class TrainingRecord < ApplicationRecord
   belongs_to :user
-  has_one :form_check, dependent: :destroy
+  # has_one :form_check, dependent: :destroy
 
   validates :weight, :reps, :sets, :training_date, presence: true
   validates :weight, :reps, :sets, numericality: { greater_than: 0 }
