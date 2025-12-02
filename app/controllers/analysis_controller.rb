aa4bb2a3d9622961f44508c328223414ff550298
ryouma21 <zuotenglengzhen58@gmail.com>
2025-12-02 12:49:18 +0900

    analysisページに必要なデータを渡す処理を追加

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -1,4 +1,15 @@
 class AnalysisController < ApplicationController
   def index
+    # ① 過去の記録
+    @training_records = current_user.training_records.order(training_date: :asc)
+
+    # ② 1RM配列
+    @one_rm_values = @training_records.map { |r| r.estimated_one_rm }.compact
+
+    # ③ 日付配列
+    @one_rm_dates = @training_records.map { |r| r.training_date.strftime("%Y/%m/%d") }
+
+    # ④ 週間総ボリューム
+    @weekly_volume = TrainingRecord.weekly_volume(current_user)
   end
 end
