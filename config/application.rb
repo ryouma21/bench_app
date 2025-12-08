25dfdb641caa856dce73df3f129b0ec4de585f21
ryouma21 <zuotenglengzhen58@gmail.com>
2025-12-08 12:24:17 +0900

    Devise翻訳ファイルとアプリ全体の日本語化設定を追加

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module BenchApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    
+    # Rails 全体を日本語化する設定
+    config.i18n.default_locale = :ja
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
